88b1a36d4839478b23444f6c97ba97d4a16856c0
dixin-wan <dixin.wan@uconn.edu>
2022-06-13 01:13:23 -0400

    p

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import{ ChatEngine }from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () =>{
     return(
         <ChatEngine
              height="100vh"
-             projectID=""
-             userName=""
-             userSecret=""
+             projectID="cd69d381-af4b-4128-9162-9d1b31a72210"
+             userName="XynthiaWan"
+             userSecret="123123"
+             renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}
+             />}
 
         />
-    )
+    );
 }
 
 
